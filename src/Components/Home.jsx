7c871dd88a4ff20428f437a69ea1c5589b846ece
Jesse Brown <jnbentum@gmail.com>
2025-12-05 22:12:46 +0000

    Fix import path for data/products.js

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 // src/pages/Home.jsx
 import React from "react";
 import Hero from "../Components/Hero";
-import products from "../data/products";
+import products from "../data/products.js";
 import ProductGrid from "../Components/ProductGrid";
 
 const Home = () => {
